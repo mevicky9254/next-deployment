3edff390724ce9bdd39ad1cc5030b55a34019ca4
vikki <vikki@right2vote.in>
2025-11-05 21:12:59 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enables static HTML export
+  images: {
+    unoptimized: true, // For static builds
+  },
+  trailingSlash: true, // Recommended for static hosting
 };
 
 export default nextConfig;
